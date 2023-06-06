81f4de8902872354038c47ae98220c7bf1aca8b8
9000HAL <gsanchezscs@gmail.com>
2023-06-06 13:16:17 -0700

    working ex. #2

--- JS-day1-HW.js
+++ JS-day1-HW.js
@@ -48,10 +48,27 @@ function findWords(dog_string, dog_names) {
 /*Write a function that takes in an array and removes every even index with a splice,
 and replaces it with the string "even index" */
 
-Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 
-function replaceEvens(arr){
-    //code goes here
+//function replaceEvens(arr){
+    //"code goes here"
+    // Define the function that replaces even indexes with "even index"
+function replaceEvens(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2 === 0) {
+        arr.splice(i, 1, "even index");
+      }
+    }
+    return arr;
+  }
+  
+  // Test the function with the provided parameter
+  let arr = ["Max", "Baseball", "Reboot", "Goku", "Trucks", "Rodger"];
+  let outputArr = replaceEvens(arr);
+  console.log("Given arr == ", arr);
+  console.log("Output arr == ", outputArr);
+  
+
 }
 
 //Expected output
